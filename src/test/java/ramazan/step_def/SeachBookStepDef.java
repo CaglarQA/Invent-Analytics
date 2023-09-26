53929cbaf3b4a8d57c4c4b610be444f19a6ba5f4
Caglar Engin <smyrna.caglar@gmail.com>
2023-09-27 00:23:19 +0200

    Second Task -completed succesfully

--- src/test/java/ramazan/step_def/SeachBookStepDef.java
+++ src/test/java/ramazan/step_def/SeachBookStepDef.java
@@ -2,8 +2,10 @@ package ramazan.step_def;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import ramazan.pages.BooksPage;
 import ramazan.pages.LoginLogoutPage;
+import ramazan.utily.ConfigurationReader;
 
 public class SeachBookStepDef {
 
@@ -11,13 +13,20 @@ public class SeachBookStepDef {
     LoginLogoutPage loginLogoutPage=new LoginLogoutPage();
 
     @When("user enters {string} in the search box")
-    public void user_enters_in_the_search_box(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_enters_in_the_search_box(String keyword) {
+        keyword= ConfigurationReader.getProperty("keyword");
+        booksPage.inputSearchBox.sendKeys(keyword);
+
+
     }
     @Then("user see the book containing {string}")
-    public void user_see_the_book_containing(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_see_the_book_containing(String keyword) {
+        keyword= ConfigurationReader.getProperty("keyword");
+        System.out.println("keyword = " + keyword);
+
+        String searchedBook=booksPage.searchedBook.getText();
+        System.out.println("searchedBook = " + searchedBook);
+        Assert.assertTrue(searchedBook.toLowerCase().contains(keyword.toLowerCase()));
+
     }
 }
